e8806667b276e67febe175c98b364aff64d94d43
AdrianFoxy <drugakadrian@gmail.com>
2023-05-03 15:45:17 +0300

    InitialCommit, TwoSum and DiffOfArrays tasks

---
+++ LeetCodeTasks/Find the Difference of Two Arrays/FindTheDiffOfTwoArrays.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.Find_the_Difference_of_Two_Arrays
+{
+    public static class FindTheDiffOfTwoArrays
+    {
+        // https://leetcode.com/problems/find-the-difference-of-two-arrays/
+        public static IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+        {
+            return new[] {
+                 nums1.Except(nums2).ToList(),
+                 nums2.Except(nums1).ToList()
+            };
+        }
+
+    }
+}
---
+++ LeetCodeTasks/Program.cs
@@ -0,0 +1,32 @@
+﻿using LeetCodeTasks.Find_the_Difference_of_Two_Arrays;
+using LeetCodeTasks.TwoSum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TwoSum test
+            /*int[] TwoSumAnswer = SolutionTwoSum.TwoSum(new int[] { 3, 2, 4 }, 6);
+            Console.WriteLine(string.Join(", ", TwoSumAnswer));*/
+
+            // DiffOfArrays test
+            /*IList<IList<int>> FindTheDiffOfArrs = FindTheDiffOfTwoArrays.FindDifference(new int[] { 1, 5, 3, 2 }, new int[] { 1, 5, 3, 4 });
+            foreach (IList<int> list in FindTheDiffOfArrs)
+            {
+                foreach (int item in list)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+            Console.WriteLine();*/
+
+        }
+    }
+}
