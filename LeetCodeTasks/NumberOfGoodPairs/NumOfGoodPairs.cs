cf7664845d973e1b81404e37d37965319914161b
AdrianFoxy <drugakadrian@gmail.com>
2023-05-03 16:36:36 +0300

    NumberOfGoodPairs

---
+++ LeetCodeTasks/NumberOfGoodPairs/NumOfGoodPairs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.NumberOfGoodPairs
+{
+    // https://leetcode.com/problems/number-of-good-pairs/submissions/943817562/
+    public static class NumOfGoodPairs
+    {
+        public static int NumIdenticalPairs(int[] nums)
+        {
+            int goodpairs = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j]) goodpairs++;
+                }
+            }
+            return goodpairs;
+        }
+    }
+}
